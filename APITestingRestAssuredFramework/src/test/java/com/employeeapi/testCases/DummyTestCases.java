d92f99fd0610ccea213db2b7180300576c1ac019
mdsharifuzzaman <mdsharifuzzaman@mds-macbook-pro>
2019-11-14 22:49:01 -0500

    Adding New DummyTestCases

---
+++ APITestingRestAssuredFramework/src/test/java/com/employeeapi/testCases/DummyTestCases.java
@@ -0,0 +1,13 @@
+package com.employeeapi.testCases;
+
+import org.testng.annotations.Test;
+
+import com.employeeapi.base.TestBase;
+
+public class DummyTestCases extends TestBase {
+	
+	@Test
+	void myName() {
+	logger.info("******************** My Name is rBn Khan ********************");
+	}
+}
